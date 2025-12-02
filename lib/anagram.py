9508e0f16071bac95dc9b0d03627a494f7ee5744
Ephy <macbookpro@Ephys-MacBookPro.local>
2025-12-02 07:34:56 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for w in word_list:
+            if sorted(w.lower()) == self.sorted_word and w.lower() != self.word:
+                matches.append(w)
+        return matches
